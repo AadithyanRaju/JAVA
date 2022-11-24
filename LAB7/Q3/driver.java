9b80823dbf5e6be2fac065bfdca2144321384b79
AadithyanRaju <aadithyan75@gmail.com>
2022-11-24 11:45:37 +0530

    l7

---
+++ LAB7/Q3/driver.java
@@ -0,0 +1,18 @@
+package LAB7.Q3;
+
+import java.util.Scanner;
+
+public class driver {
+    public static void main(String[] args) {
+        String s1;
+        Scanner scan=new Scanner(System.in);
+        System.out.println("enter string : ");
+        s1=scan.next();
+        scan.close();
+        StringBuffer s=new StringBuffer(s1);
+        StringBuffer s2=new StringBuffer(s);
+        s2.reverse();
+        if(s2.compareTo(s)==0) System.out.println("YES");
+        else System.out.println("NO");
+    }
+}
